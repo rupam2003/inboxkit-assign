fe8090d58d47aa5f03a799635d94e133349367bf
rupam2003 <rupambhattacharya0@gmail.com>
2026-02-14 18:05:20 +0530

    project basics done

--- convex/init.ts
+++ convex/init.ts
@@ -1,13 +1,12 @@
 import { internalMutation } from "./_generated/server";
 
 export const intialize = internalMutation({
-  args:{},
-  handler: async (ctx) => {
-    for (let index = 0; index < 5000; index++) {
-      await ctx.db.insert("blocks",{
-        index,
-      })      
-    } 
-  }
-  
-})
+	args: {},
+	handler: async (ctx) => {
+		for (let index = 0; index < 2000; index++) {
+			await ctx.db.insert("blocks", {
+				index,
+			});
+		}
+	},
+});
